7cb3e7a25fdb0fb04ef68cad9fbddb191e013da2
Daniyal Ahmed <daniahmedkhatri@gmail.com>
2019-10-14 11:29:27 +0500

    description added

---
+++ sumlib/target/package/sumlib-0.1.0/target/debug/deps/sumlib-4e49f0ab82b0dd65.d
@@ -0,0 +1,7 @@
+D:\learning rust\sumlib\target\package\sumlib-0.1.0\target\debug\deps\sumlib-4e49f0ab82b0dd65.rmeta: src\lib.rs
+
+D:\learning rust\sumlib\target\package\sumlib-0.1.0\target\debug\deps\libsumlib-4e49f0ab82b0dd65.rlib: src\lib.rs
+
+D:\learning rust\sumlib\target\package\sumlib-0.1.0\target\debug\deps\sumlib-4e49f0ab82b0dd65.d: src\lib.rs
+
+src\lib.rs:
---
+++ sumlib/target/package/sumlib-0.1.0/target/debug/libsumlib.d
@@ -0,0 +1,1 @@
+D:\learning\ rust\sumlib\target\package\sumlib-0.1.0\target\debug\libsumlib.rlib: D:\learning\ rust\sumlib\target\package\sumlib-0.1.0\src\lib.rs
